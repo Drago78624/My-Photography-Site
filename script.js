593f82ac77f35b0dc702f70280bbeaa24c322db1
Muhammad Maaz Ahmed <maazahmed78624@gmail.com>
2021-11-07 18:10:19 +0500

    Photography Site 3.0 (latest)

--- script.js
+++ script.js
@@ -6,49 +6,83 @@ document.getElementsByClassName("font-changer__poppins")[0].addEventListener("cl
     document.getElementsByTagName("body")[0].style.fontFamily = "'Poppins', sans-serif"
 })
 
-document.getElementsByClassName("theme-changer-purple")[0].addEventListener("click" , ()=>{
-    document.querySelectorAll("body div .purple").forEach(element => {
-        element.style.backgroundColor = "purple";
-    });
-})
+//Function for changing theme to purple
 
-document.getElementsByClassName("theme-changer-purple")[0].addEventListener("click" , ()=>{
+function purple(){
     document.querySelectorAll("body div .purple-text").forEach(element => {
         element.style.color = "purple";
     });
-})
-
-document.getElementsByClassName("theme-changer-red")[0].addEventListener("click" , ()=>{
     document.querySelectorAll("body div .purple").forEach(element => {
-        element.style.backgroundColor = "orangered";
+        element.style.backgroundColor = "purple";
     });
-})
+}
 
-document.getElementsByClassName("theme-changer-red")[0].addEventListener("click" , ()=>{
+//Function for changing theme to orange
+
+function orange(){
     document.querySelectorAll("body div .purple-text").forEach(element => {
         element.style.color = "orangered";
     });
-})
+    document.querySelectorAll("body div .purple").forEach(element => {
+        element.style.backgroundColor = "orangered";
+    });
+}
 
-document.getElementsByClassName("defaults")[0].addEventListener("click" , ()=>{
+//Function for default style
+
+function defaults(){
     document.querySelectorAll("body div .purple").forEach(element => {
         element.style.backgroundColor = "#A91A12";
     });
-})
-
-document.getElementsByClassName("defaults")[0].addEventListener("click" , ()=>{
     document.querySelectorAll("body div .purple-text").forEach(element => {
         element.style.color = "#AE493E";
     });
-})
-
-
-document.getElementsByClassName("defaults")[0].addEventListener("click" , ()=>{
     document.querySelectorAll("body div .orangered").forEach(element => {
-        element.style.backgroundColor = "red";
+        element.style.backgroundColor = "#A91A12";
     });
-})
-document.getElementsByClassName("defaults")[0].addEventListener("click" , ()=>{
     document.getElementsByTagName("body")[0].style.fontFamily = "'Roboto', sans-serif"
-})
+}
+
+function light_mode(){
+    document.querySelectorAll("body div .black-text").forEach(element => {
+        element.classList.replace("black-text" , "black-text-class");
+    });
+    document.querySelectorAll("body .white-background").forEach(element => {
+        element.classList.replace("white-background" , "white-background-class");
+    });
+    document.querySelectorAll("body .light-gray-background").forEach(element => {
+        element.classList.replace("light-gray-background" , "light-gray-background-class");
+    });
+    document.querySelectorAll("body div .gray-text").forEach(element => {
+        element.classList.replace("gray-text" , "gray-text-class");
+    });
+    document.querySelectorAll("body div .dark-gray-text").forEach(element => {
+        element.classList.replace("dark-gray-text" , "dark-gray-text-class");
+    });
+    document.querySelectorAll("body .dark-gray-background").forEach(element => {
+        element.classList.replace("dark-gray-background" , "dark-gray-background-class");
+    });
+}
+
+function dark_mode(){
+    document.querySelectorAll("body div .black-text-class").forEach(element => {
+        element.classList.replace("black-text-class" , "black-text");
+    });
+    document.querySelectorAll("body .white-background-class").forEach(element => {
+        element.classList.replace("white-background-class" , "white-background");
+    });
+    document.querySelectorAll("body .light-gray-background-class").forEach(element => {
+        element.classList.replace("light-gray-background-class" , "light-gray-background");
+    });
+    document.querySelectorAll("body div .gray-text-class").forEach(element => {
+        element.classList.replace("gray-text-class" , "gray-text");
+    });
+    document.querySelectorAll("body div .dark-gray-text-class").forEach(element => {
+        element.classList.replace("dark-gray-text-class" , "dark-gray-text");
+    });
+    document.querySelectorAll("body .dark-gray-background-class").forEach(element => {
+        element.classList.replace("dark-gray-background-class" , "dark-gray-background");
+    });
+}
+
 
